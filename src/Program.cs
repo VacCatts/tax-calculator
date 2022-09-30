6c89240b4efbaace44195e7711df55d915630dc8
VacCat <vaccatbusiness@protonmail.com>
2022-09-30 21:33:44 +0300

    show the tax you get just in case

--- src/Program.cs
+++ src/Program.cs
@@ -34,9 +34,11 @@ namespace taxcalculator
             int betamount = int.Parse(Ibetamount) * 2;
             double taxpercentage = double.Parse(Itaxpercentage, System.Globalization.CultureInfo.InvariantCulture);
             var result = Math.Round(betamount - ((taxpercentage / 100) * betamount));
+            var youget = betamount - result;
 
             // assuming the calculation worked, print it
             Console.WriteLine($"give the winner {result}wls");
+            Console.WriteLine($"you get {youget}wls tax");
             Console.WriteLine("press any key to go back");
             Console.ReadKey();
             Menu();
